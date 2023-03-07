eb6a1f7fda076d6103efb9c0eaa104feb595fe5e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-07 15:59:57 +0100

    Hello Andriod!

--- app/src/main/c/MainActivity.c
+++ app/src/main/c/MainActivity.c
@@ -1,1 +1,29 @@
+#include <assert.h>
 #include <jni.h>
+#include <pear.h>
+#include <uv.h>
+
+#include "MainActivity.bundle.h"
+
+JNIEXPORT void JNICALL
+Java_com_holepunch_pear_MainActivity_init (JNIEnv *env, jobject self) {
+  int err;
+
+  int argc = 0;
+  char **argv = NULL;
+
+  argv = uv_setup_args(argc, argv);
+
+  pear_t pear;
+  err = pear_setup(uv_default_loop(), &pear, argc, argv);
+  assert(err == 0);
+
+  uv_buf_t source = uv_buf_init((char *) bundle, bundle_len);
+
+  err = pear_run(&pear, "/main.bundle", &source);
+  assert(err == 0);
+
+  int exit_code;
+  err = pear_teardown(&pear, &exit_code);
+  assert(err == 0);
+}
--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,5 +1,19 @@
 package com.holepunch.pear;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {}
+public class MainActivity extends Activity {
+  static {
+    System.loadLibrary("pear_android_shared");
+  }
+
+  public native void init();
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    init();
+  }
+}
