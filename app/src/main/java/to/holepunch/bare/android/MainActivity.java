a3c63fa92f754fd08abd021c817576cf15e98d52
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-06-27 15:11:35 +0200

    Switch to Bare Kit (#2)

---
+++ app/src/main/java/to/holepunch/bare/android/MainActivity.java
@@ -0,0 +1,59 @@
+package to.holepunch.bare.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import to.holepunch.bare.kit.IPC;
+import to.holepunch.bare.kit.Worklet;
+
+public class MainActivity extends Activity {
+  static {
+    System.loadLibrary("bare_android");
+  }
+
+  Worklet worklet;
+
+  @Override
+  public void
+  onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    worklet = new Worklet();
+
+    try {
+      worklet.start("/app.bundle", getAssets().open("app.bundle"));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    IPC ipc = new IPC(worklet);
+
+    ipc.read("UTF-8", (data, exception) -> Log.d("bare", data));
+    ipc.write("Hello from Android", "UTF-8");
+  }
+
+  @Override
+  public void
+  onPause () {
+    super.onPause();
+
+    worklet.suspend();
+  }
+
+  @Override
+  public void
+  onResume () {
+    super.onResume();
+
+    worklet.resume();
+  }
+
+  @Override
+  public void
+  onDestroy () {
+    super.onDestroy();
+
+    worklet.terminate();
+    worklet = null;
+  }
+}
