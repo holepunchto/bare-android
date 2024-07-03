8f0364639ff6737412a0052b1a50cd3ebb3ac852
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-07-03 11:17:38 +0200

    Use RPC

--- app/src/main/js/app.js
+++ app/src/main/js/app.js
@@ -1,10 +1,20 @@
-/* global Bare */
+/* global Bare, BareKit */
 
 Bare
   .on('suspend', () => console.log('suspended'))
   .on('resume', () => console.log('resumed'))
   .on('exit', () => console.log('exited'))
 
-Bare.IPC
-  .on('data', (data) => console.log(data.toString()))
-  .write('Hello from Bare')
+const rpc = new BareKit.RPC((req) => {
+  if (req.command === 'ping') {
+    console.log(req.data.toString())
+
+    req.reply('Pong from Bare')
+  }
+})
+
+const req = rpc.request('ping')
+
+req.send('Ping from Bare')
+
+req.reply().then((data) => console.log(data.toString()))
