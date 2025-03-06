e0652789a655bca3503881d069a1251e3803636f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-03-06 10:14:23 +0100

    Adjust logs

--- app/src/main/js/push.js
+++ app/src/main/js/push.js
@@ -1,13 +1,13 @@
-console.log('Hello from push worklet!')
+console.log('Hello Android notifcations!')
 
 BareKit.on('push', (json, reply) => {
-  console.log('Push notification received:', json.toString())
+  console.log('Notification received:', json)
 
   reply(
     null,
     JSON.stringify({
-      title: 'Push notification received',
-      body: 'this is the body'
+      title: 'Notification received',
+      body: 'This is the body'
     })
   )
 })
