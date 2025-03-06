4564cbcbe1b7efb137d0b97d5b5c676cf08cc457
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-03-06 09:22:02 +0100

    Run Prettier

--- app/src/main/assets/push.js
+++ app/src/main/assets/push.js
@@ -1,5 +1,13 @@
 console.log('Hello from push worklet!')
-BareKit.on('push', function(json, reply) {
+
+BareKit.on('push', (json, reply) => {
   console.log('Push notification received:', json.toString())
-  reply(null, JSON.stringify({ title: "Push notification received", body: "this is the body" }))
+
+  reply(
+    null,
+    JSON.stringify({
+      title: 'Push notification received',
+      body: 'this is the body'
+    })
+  )
 })
