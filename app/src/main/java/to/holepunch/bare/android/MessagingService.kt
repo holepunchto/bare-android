dac39160de0d2ced63cbd49fdd47eb3e60771f8e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-03-06 10:11:22 +0100

    Remove compat layer from messaging service

--- app/src/main/java/to/holepunch/bare/android/MessagingService.kt
+++ app/src/main/java/to/holepunch/bare/android/MessagingService.kt
@@ -1,26 +1,22 @@
 package to.holepunch.bare.android
 
 import android.R.drawable
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.util.Log
-import androidx.core.app.NotificationCompat
 import org.json.JSONObject
-import to.holepunch.bare.kit.MessagingService as BaseMessagingService
 import to.holepunch.bare.kit.Worklet
-import java.io.IOException
+import to.holepunch.bare.kit.MessagingService as BaseMessagingService
 
 class MessagingService : BaseMessagingService(Worklet.Options()) {
   private var notificationManager: NotificationManager? = null
 
-  init {
-    Log.v("CustomMessagingService", "Worklet created!")
-  }
-
   override fun onCreate() {
     super.onCreate()
 
-    notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager = getSystemService(NotificationManager::class.java)
+
     notificationManager!!.createNotificationChannel(
       NotificationChannel(
         CHANNEL_ID,
@@ -29,36 +25,31 @@ class MessagingService : BaseMessagingService(Worklet.Options()) {
       )
     )
 
-    val context = applicationContext
-    val assetManager = context.assets
-
     try {
-      this.start("app.bundle", assetManager.open("push.bundle"), null)
-      Log.v("CustomMessagingService", "Worklet started!")
-    } catch (e: IOException) {
-      Log.e("CustomMessagingService", "Failed to start worklet", e)
+      this.start("/push.bundle", assets.open("push.bundle"), null)
+    } catch (e: Exception) {
+      throw RuntimeException(e)
     }
   }
 
   override fun onWorkletReply(reply: JSONObject) {
-    Log.v("CustomMessagingService", "json: $reply")
     try {
       notificationManager!!.notify(
-        1, NotificationCompat.Builder(this, CHANNEL_ID)
+        1,
+        Notification.Builder(this, CHANNEL_ID)
           .setSmallIcon(drawable.ic_dialog_info)
           .setContentTitle(reply.optString("title", "Default title"))
           .setContentText(reply.optString("body", "Default description"))
-          .setPriority(NotificationCompat.PRIORITY_DEFAULT)
           .setAutoCancel(true)
           .build()
       )
     } catch (e: Exception) {
-      Log.e("CustomMessagingService", "Error showing notification", e)
+      throw RuntimeException(e)
     }
   }
 
   override fun onNewToken(token: String) {
-    Log.v("CustomMessagingService", "token: $token")
+    Log.v("MessagingService", "Token: $token")
   }
 
   companion object {
