2aa7dc6afcaeeefc44469aab2055b9688af753c3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-04-07 12:58:44 +0200

    Put companion object last

--- app/src/main/java/to/holepunch/bare/android/voip/ConnectionService.kt
+++ app/src/main/java/to/holepunch/bare/android/voip/ConnectionService.kt
@@ -9,6 +9,23 @@ import android.telecom.Connection as BaseConnection
 import android.telecom.ConnectionService as BaseConnectionService
 
 class ConnectionService : BaseConnectionService() {
+  override fun onCreateIncomingConnection(
+    connectionManagerPhoneAccount: PhoneAccountHandle?,
+    request: ConnectionRequest?
+  ): Connection {
+    val id = request?.extras?.getString("CONNECTION_ID")!!
+    val caller = request.extras?.getString("CALLER_NAME")!!
+
+    val conn = Connection(applicationContext, caller, id)
+
+    conn.setConnectionProperties(BaseConnection.PROPERTY_SELF_MANAGED)
+    conn.setAudioModeIsVoip(true)
+
+    connections[id] = conn
+
+    return conn
+  }
+
   companion object {
     val connections: MutableMap<String, Connection> = mutableMapOf()
 
@@ -30,22 +47,5 @@ class ConnectionService : BaseConnectionService() {
 
       notificationManager.cancel(10)
     }
-  }
-
-  override fun onCreateIncomingConnection(
-    connectionManagerPhoneAccount: PhoneAccountHandle?,
-    request: ConnectionRequest?
-  ): Connection {
-    val id = request?.extras?.getString("CONNECTION_ID")!!
-    val caller = request.extras?.getString("CALLER_NAME")!!
-
-    val conn = Connection(applicationContext, caller, id)
-
-    conn.setConnectionProperties(BaseConnection.PROPERTY_SELF_MANAGED)
-    conn.setAudioModeIsVoip(true)
-
-    connections[id] = conn
-
-    return conn
   }
 }
